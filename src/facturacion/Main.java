cd2e32f95028212c55af30a120d69be036e1de20
Juan Pablo <rovayojuanpablo@gmail.com>
2024-12-19 23:39:48 -0500

    Init: Sistema Facturacion Creado

---
+++ src/facturacion/Main.java
@@ -0,0 +1,14 @@
+package facturacion;
+
+import ec.edu.puce.facturacion.forms.FrmMenuPrincipal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Pablo");
+		FrmMenuPrincipal mainForm = new FrmMenuPrincipal();
+		mainForm.setVisible(true);
+		
+	}
+
+}
